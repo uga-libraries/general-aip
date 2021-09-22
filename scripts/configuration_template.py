1262e0008124fe26c16769328bf552ecf9b0b449
Adriane Hanson <ahansonuga@gmail.com>
2021-09-22 16:10:56 -0400

    Removed prepare_bag from configuration_template
    This script is no longer used. Replaced by a python function to tar and zip.

--- scripts/configuration_template.py
+++ scripts/configuration_template.py
@@ -1,11 +1,10 @@
 # Local paths to programs used in the script
-#   * fits: Mac/Linux use fits.sh, Windows use fits.bat
-#   * prepare_bag_script: Mac/Linux use prepare_bag, Windows use 7zprepare_bag.pl
+
+# For fits: Mac/Linux use fits.sh, Windows use fits.bat
 
 # In a Windows environment, change the / to \\
 
 fits = 'C:/INSERT/PATH/Fits/fits-#.#.#/fits.EXT'
 saxon = 'C:/INSERT/PATH/SaxonHE#-#/saxon-he-#.#.jar'
 md5deep = 'C:/INSERT/PATH/md5deep-#.#/md5deep64.exe'
-prepare_bag_script = 'C:/INSERT/PATH/SCRIPTNAME'
 stylesheets = 'C:/INSERT/PATH/stylesheets'

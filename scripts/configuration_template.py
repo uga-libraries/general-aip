db90a9ee65dfe17387ee31d484570e1916f2aeea
Adriane Hanson <ahansonuga@gmail.com>
2021-09-22 16:20:51 -0400

    Add usage instructions to configuration_template

--- scripts/configuration_template.py
+++ scripts/configuration_template.py
@@ -1,4 +1,10 @@
-# Local paths to programs used in the script
+# Local paths to programs used in the script. 
+# The first three are dependencies and the stylesheets folder is part of this GitHub repo.
+
+# To use, make a copy of this file named configuration.py and update the paths to match your local machine.
+#     C: may need to be replaced with a different drive number.
+#     INSERT\\PATH\\ is replaced with the path on your local machine.
+#     # is replaced by the number for your version of the programs.
 
 # In a Mac or Linux environment:
 #     For fits, use the path to fits.sh instead of fits.bat

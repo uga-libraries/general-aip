f4501b1138aab9e88521ad66f127285cfc785ece
Adriane Hanson <ahansonuga@gmail.com>
2021-09-30 15:23:06 -0400

    Update configuration_template.py
    Clarified notes. Made variable names uppercase to match Python convention. Simplified example paths.

--- scripts/configuration_template.py
+++ scripts/configuration_template.py
@@ -1,18 +1,18 @@
-# Local paths to programs used in the script. 
+# Local paths to programs used in the aip functions.
 # The first three are dependencies and the stylesheets folder is part of this GitHub repo.
 
-# To use, make a copy of this file named configuration.py and update the paths to match your local machine.
-#     C: may need to be replaced with a different drive number.
-#     INSERT\\PATH\\ is replaced with the path on your local machine.
-#     # is replaced by the number for your version of the programs.
+"""
+To use, make a copy of this file named configuration.py and update the paths to match your local machine.
+    C: may need to be replaced with a different drive letter.
+    INSERT\\PATH\\ is replaced with the path on your local machine.
+    # is replaced by the number for your version of the programs.
 
-# In a Mac or Linux environment:
-#     For fits, use the path to fits.sh instead of fits.bat
-#     Change \\ to /
+In a Mac or Linux environment:
+    Use the path to fits.sh instead of fits.bat
+    Change \\ to /
+"""
 
-# In a Mac or Linux environment, change the \\ to /
-
-fits = 'C:\\INSERT\\PATH\\Fits\\fits-#.#.#\\fits.bat'
-saxon = 'C:\\INSERT\\PATH\\SaxonHE#-#\\saxon-he-#.#.jar'
-md5deep = 'C:\\INSERT\\PATH\\md5deep-#.#\\md5deep64.exe'
-stylesheets = 'C:\\INSERT\\PATH\\stylesheets'
+FITS = 'C:\\INSERT\\PATH\\fits.bat'
+SAXON = 'C:\\INSERT\\PATH\\saxon-he-#.#.jar'
+MD5DEEP = 'C:\\INSERT\\PATH\\md5deep64.exe'
+STYLESHEETS = 'C:\\INSERT\\PATH\\stylesheets'

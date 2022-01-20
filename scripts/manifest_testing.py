7c90cce1821f35a6fa6a5e047f603ca179350966
Adriane M Hanson <ahansonuga@gmail.com>
2022-01-20 16:36:09 -0500

    Create manifest_testing.py
    Temporary script for testing how the manifest text file is made, which is causing an error when uploaded to our preservation system.

---
+++ scripts/manifest_testing.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+import configuration as c
+
+os.chdir("insert-path")
+
+for file in os.listdir('.'):
+    output = subprocess.run(f'"{c.MD5DEEP}" -br "{file}"', stdout=subprocess.PIPE, shell=True)
+
+    # Blank line between each row.
+    m0 = os.path.join(f"../aips-to-ingest", f"manifest_default.txt")
+    with open(m0, 'a', encoding='utf-8') as m0file:
+        m0file.write(output.stdout.decode("UTF-8"))
+
+    # Blank line between each row.
+    m1 = os.path.join(f"../aips-to-ingest", f"manifest_no_r.txt")
+    with open(m1, 'a', encoding='utf-8') as m1file:
+        m1file.write(output.stdout.decode("UTF-8").rstrip("\r"))
+
+    # No blank line. Causes error in ARCHive unless copy to a new text file manually.
+    m2 = os.path.join(f"../aips-to-ingest", f"manifest_no_n.txt")
+    with open(m2, 'a', encoding='utf-8') as m2file:
+        m2file.write(output.stdout.decode("UTF-8").rstrip("\n"))
+
+    # Everything is merged to a single line.
+    m3 = os.path.join(f"../aips-to-ingest", f"manifest_no_nr.txt")
+    with open(m3, 'a', encoding='utf-8') as m3file:
+        m3file.write(output.stdout.decode("UTF-8").rstrip("\r\n"))
+
+    # No blank link. ARCHive can read this. Want something simpler though?
+    m4 = os.path.join(f"../aips-to-ingest", f"manifest_add n back.txt")
+    with open(m4, 'a', encoding='utf-8') as m4file:
+        m4file.write(output.stdout.decode("UTF-8").rstrip("\r\n") + "\n")
+

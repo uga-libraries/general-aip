f50780f0d908e78e3aeab0c685acfa18a9e4c901
Adriane Hanson <ahansonuga@gmail.com>
2022-10-14 09:05:22 -0400

    test_check_arguments.py: setUp metadata.csv
    So the test are more granular, have metadata.csv in the correct location for all tests except the one that detects if it is missing or not.

--- tests/test_check_arguments.py
+++ tests/test_check_arguments.py
@@ -8,6 +8,15 @@ from scripts.aip_functions import check_arguments
 
 class TestCheckArguments(unittest.TestCase):
 
+    def setUp(self):
+        """
+        Makes a file named metadata.csv in the AIPs directory, which is expected by the script.
+        This file doesn't have the correct content, but having the correct file name is enough for these tests.
+        """
+        self.csv_path = os.path.join(os.getcwd(), 'metadata.csv')
+        with open(self.csv_path, "w") as file:
+            file.write("File for testing metadata.csv is present")
+
     def test_one_arg(self):
         """
         Test for if the user supplies no arguments. There is still one argument in sys.argv, the script name.
@@ -21,21 +30,9 @@ class TestCheckArguments(unittest.TestCase):
         """
         Test for if the user supplies a valid aips_directory and the metadata.csv is also in the expected location.
         """
-        metadata_csv_path = os.path.join(os.getcwd(), 'metadata.csv')
-        with open(metadata_csv_path, "w") as file:
-            file.write("Sample file")
         result = check_arguments(['general-aip.py', os.getcwd()])
-        expected = (os.getcwd(), True, metadata_csv_path, [])
+        expected = (os.getcwd(), True, self.csv_path, [])
         self.assertEqual(result, expected, 'Problem with correct aips_directory and correct metadata.csv')
-        os.remove(metadata_csv_path)
-
-    def test_two_arg_metadata_err(self):
-        """
-        Test for if the user supplies a valid aips_directory and the metadata.csv is not in the expected location.
-        """
-        result = check_arguments(['general-aip.py', os.getcwd()])
-        expected = (os.getcwd(), True, None, ['Missing the required file metadata.csv in the AIPs directory.'])
-        self.assertEqual(result, expected, 'Problem with correct aips_directory and missing metadata.csv')
 
     def test_two_arg_aips_err(self):
         """
@@ -46,23 +43,31 @@ class TestCheckArguments(unittest.TestCase):
                                        'Cannot check for the metadata.csv in the AIPs directory because the AIPs directory has an error.'])
         self.assertEqual(result, expected, 'Problem with incorrect aips_directory')
 
+    def test_two_arg_metadata_err(self):
+        """
+        Test for if the user supplies a valid aips_directory and the metadata.csv is not in the expected location.
+        """
+        os.remove(self.csv_path)
+        result = check_arguments(['general-aip.py', os.getcwd()])
+        expected = (os.getcwd(), True, None, ['Missing the required file metadata.csv in the AIPs directory.'])
+        self.assertEqual(result, expected, 'Problem with correct aips_directory and missing metadata.csv')
+
     def test_three_arg_no_err(self):
         """
         Test for if the user includes the correct optional argument, no-zip.
-        The aips_directory is correct and metadata.csv is missing (easier test to not have to generate that file).
+        The aips_directory is correct and metadata.csv is in the expected location.
         """
         result = check_arguments(['general-aip.py', os.getcwd(), 'no-zip'])
-        expected = (os.getcwd(), False, None, ['Missing the required file metadata.csv in the AIPs directory.'])
+        expected = (os.getcwd(), False, self.csv_path, [])
         self.assertEqual(result, expected, 'Problem with correct no-zip')
 
     def test_three_arg_err(self):
         """
         Test for if the user includes the optional argument but it is not the expected value.
-        The aips_directory is correct and metadata.csv is missing (easier to test to not have to generate that file).
+        The aips_directory is correct and metadata.csv is in the expected location.
         """
         result = check_arguments(['general-aip.py', os.getcwd(), 'error'])
-        expected = (os.getcwd(), None, None, ['Unexpected value for the second argument. If provided, should be "no-zip".',
-                                              'Missing the required file metadata.csv in the AIPs directory.'])
+        expected = (os.getcwd(), None, self.csv_path, ['Unexpected value for the second argument. If provided, should be "no-zip".'])
         self.assertEqual(result, expected, 'Problem with wrong value for no-zip argument')
 
 

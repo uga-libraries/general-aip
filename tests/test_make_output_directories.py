f0bf2dd637a6e6b05f927e856e0a32b3869e9e2f
Adriane Hanson <ahansonuga@gmail.com>
2022-11-07 16:38:56 -0500

    test_make_output_directories.py: better comparison
    Using the change in directories within the parent folder instead of testing for if the three exist.
    
    Also added tearDown to remove the output directories rather than including them within the test so they still work even if the assert has an error, to prevent a problem with the next test.

--- tests/test_make_output_directories.py
+++ tests/test_make_output_directories.py
@@ -8,22 +8,27 @@ from scripts.aip_functions import make_output_directories
 
 class TestMakeOutputDirectories(unittest.TestCase):
 
+    def tearDown(self):
+        """
+        Deletes the three output folders.
+        """
+        os.rmdir(os.path.join('..', 'aips-to-ingest'))
+        os.rmdir(os.path.join('..', 'fits-xml'))
+        os.rmdir(os.path.join('..', 'preservation-xml'))
+
     def test(self):
         """
         Test for running the function.
-        The result to be tested is the folders in the parent directory of the current directory after the function runs.
+        The result to be tested is the new folders in the parent directory of the current directory
+        after the function runs.
         """
+        parent_directory_before = os.listdir('..')
         make_output_directories()
-        result = []
-        parent = os.path.dirname(os.getcwd())
-        for item in os.listdir(parent):
-            if item in ('aips-to-ingest', 'fits-xml', 'preservation-xml'):
-                result.append(item)
+        parent_directory_after = os.listdir('..')
+        result = list(set(parent_directory_after) - set(parent_directory_before))
+        result.sort()
         expected = ['aips-to-ingest', 'fits-xml', 'preservation-xml']
         self.assertEqual(result, expected, 'Problem with make_output_directories')
-        os.rmdir(os.path.join(parent, 'aips-to-ingest'))
-        os.rmdir(os.path.join(parent, 'fits-xml'))
-        os.rmdir(os.path.join(parent, 'preservation-xml'))
 
 
 if __name__ == '__main__':

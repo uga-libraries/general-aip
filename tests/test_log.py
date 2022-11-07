8dc635a699077049614855313ba2954c41f12f72
Adriane Hanson <ahansonuga@gmail.com>
2022-11-07 14:55:41 -0500

    test_log.py: make test_one_aip work with timestamp
    Have to use the time information automatically generated in the log when the AIP instance is made in the expected results as well.

--- tests/test_log.py
+++ tests/test_log.py
@@ -5,6 +5,7 @@ The log is updated after each step in the script.
 These tests are for if information is written to a CSV correctly.
 They are not testing if the correct information is saved to the log after each step."""
 
+import datetime
 import os
 import unittest
 from scripts.aip_functions import AIP, log
@@ -31,20 +32,21 @@ class MyTestCase(unittest.TestCase):
     def test_one_aip(self):
         """
         Test for making the log file with information from one AIP.
+        To allow for exact comparison, gets the time from the log which should be in the expected results.
         To save time, updates some items in the AIP's log variable without running the workflow steps.
         """
+        time_stamp = datetime.datetime.now()
         log('header')
         aip = AIP(os.getcwd(), 'test', 'coll-1', 'one-tar-folder', 'one-tar', 'title', 1, to_zip=False)
+        aip.log['Started'] = time_stamp
         aip.log['Deletions'] = 'No files deleted'
         aip.log['ObjectsError'] = 'Objects folder already exists in original files'
         aip.log['Complete'] = 'Error during processing'
         log(aip.log)
-        # TODO: the first column is a time stamp, so can't do an exact comparison without removing it.
-        # Read as a CSV so easier to edit? Time stamp is YYYY-MM-DD ##:##:##.######
         with open(os.path.join('..', 'aip_log.csv'), 'r') as file:
             result = file.readlines()
         expected = ['Time Started,AIP ID,Files Deleted,Objects Folder,Metadata Folder,FITS Tool Errors,FITS Combination Errors,Preservation.xml Made,Preservation.xml Valid,Bag Valid,Package Errors,Manifest Errors,Processing Complete\n',
-                    'Started: time,one-tar,No files deleted,Objects folder already exists in original files,n/a,n/a,n/a,n/a,n/a,n/a,n/a,n/a,Error during processing\n']
+                    f'{aip.log["Started"]},one-tar,No files deleted,Objects folder already exists in original files,n/a,n/a,n/a,n/a,n/a,n/a,n/a,n/a,Error during processing\n']
         self.assertEqual(expected, result, 'Problem with one aip')
 
 

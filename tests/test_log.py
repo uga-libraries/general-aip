3dce17ceb5333b8d366d905eaecb11175ac07a70
Adriane Hanson <ahansonuga@gmail.com>
2022-11-18 16:38:16 -0500

    Clean up test_log.py
    Expand function definitions and add comments.
    Start with a string for each row instead of a row.
    Add header variable to setUp.

--- tests/test_log.py
+++ tests/test_log.py
@@ -1,9 +1,8 @@
 """Testing for the function log, which takes either the string 'header' or an AIP instance
 and adds a row to the aip log with the information.
 
-The log is updated after each step in the script.
 These tests are for if information is written to a CSV correctly.
-They are not testing if the correct information is saved to the log after each step."""
+Tests for functions with error handling include testing if the correct information is saved to the log."""
 
 import os
 import unittest
@@ -11,6 +10,14 @@ from scripts.aip_functions import AIP, log
 
 
 class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        """
+        Variable with the header row value, which is used in each test.
+        """
+        self.header = 'Time Started,AIP ID,Files Deleted,Objects Folder,Metadata Folder,FITS Tool Errors,' \
+                      'FITS Combination Errors,Preservation.xml Made,Preservation.xml Valid,Bag Valid,Package Errors,' \
+                      'Manifest Errors,Processing Complete\n'
 
     def tearDown(self):
         """
@@ -21,25 +28,27 @@ class MyTestCase(unittest.TestCase):
     def test_header(self):
         """
         Test for making the log file with a header.
+        Result for testing is the content of the log file.
         """
+        # Creates the log.
         log('header')
 
+        # Result and expected are lists, with the text of each row as a list item.
+        # The test of each row is a comma-separated string combining the values from each column.
         with open(os.path.join('..', 'aip_log.csv'), 'r') as file:
             result = file.readlines()
 
-        header_expected = ['Time Started', 'AIP ID', 'Files Deleted', 'Objects Folder', 'Metadata Folder',
-                           'FITS Tool Errors', 'FITS Combination Errors', 'Preservation.xml Made',
-                           'Preservation.xml Valid', 'Bag Valid', 'Package Errors', 'Manifest Errors',
-                           'Processing Complete\n']
-        expected = [",".join(header_expected)]
+        expected = [self.header]
+
         self.assertEqual(expected, result, 'Problem with header')
 
     def test_one_aip(self):
         """
         Test for making the log file with information from one AIP.
-        To allow for exact comparison, gets the time from the log which should be in the expected results.
-        To save time, updates some items in the AIP's log variable without running the workflow steps.
+        Result for testing is the content of the log file.
         """
+        # Creates the log.
+        # To save time, updates some items in the AIP's log variable without running the workflow steps.
         log('header')
         aip = AIP(os.getcwd(), 'test', 'coll-1', 'one-tar-folder', 'one-tar', 'title', 1, to_zip=False)
         aip.log['Deletions'] = 'No files deleted'
@@ -47,26 +56,27 @@ class MyTestCase(unittest.TestCase):
         aip.log['Complete'] = 'Error during processing'
         log(aip.log)
 
+        # Result and expected are lists, with the text of each row as a list item.
+        # The test of each row is a comma-separated string combining the values from each column.
         with open(os.path.join('..', 'aip_log.csv'), 'r') as file:
             result = file.readlines()
 
-        header_expected = ['Time Started', 'AIP ID', 'Files Deleted', 'Objects Folder', 'Metadata Folder',
-                           'FITS Tool Errors', 'FITS Combination Errors', 'Preservation.xml Made',
-                           'Preservation.xml Valid', 'Bag Valid', 'Package Errors', 'Manifest Errors',
-                           'Processing Complete\n']
-        aip_expected = [str(aip.log['Started']), 'one-tar', 'No files deleted', 'Objects folder already exists in original files',
-                        'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'Error during processing\n']
-        expected = [",".join(header_expected), ",".join(aip_expected)]
+        # Time from the log is used for an exact comparison.
+        # Time changes each time the rest is run. The rest of the expected values are the same each time.
+        aip_row = f'{str(aip.log["Started"])},one-tar,No files deleted,Objects folder already exists in original ' \
+                  f'files,n/a,n/a,n/a,n/a,n/a,n/a,n/a,n/a,Error during processing\n'
+        expected = [self.header, aip_row]
+
         self.assertEqual(expected, result, 'Problem with one aip')
 
     def test_multiple_aips(self):
         """
         Test for making the log file with information from three AIPs.
-        To allow for exact comparison, gets the time from the log which should be in the expected results.
-        To save time, updates some items in the AIP's log variable without running the workflow steps.
+        Result for testing is the content of the log file.
         """
+        # Creates the log.
+        # To save time, updates some items in each AIP's log variable without running the workflow steps.
         log('header')
-
         aip1 = AIP(os.getcwd(), 'test', 'coll-1', 'aip-1-folder', 'aip-1', 'title-1', 1, to_zip=False)
         aip1.log['Deletions'] = 'No files deleted'
         aip1.log['ObjectsError'] = 'Objects folder already exists in original files'
@@ -98,27 +108,30 @@ class MyTestCase(unittest.TestCase):
         aip3.log['Complete'] = 'Successfully completed processing'
         log(aip3.log)
 
+        # Result is a list, with the text of each row as a list item.
+        # Result and expected are lists, with the text of each row as a list item.
         with open(os.path.join('..', 'aip_log.csv'), 'r') as file:
             result = file.readlines()
 
-        header_expected = ['Time Started', 'AIP ID', 'Files Deleted', 'Objects Folder', 'Metadata Folder',
-                           'FITS Tool Errors', 'FITS Combination Errors', 'Preservation.xml Made',
-                           'Preservation.xml Valid', 'Bag Valid', 'Package Errors', 'Manifest Errors',
-                           'Processing Complete\n']
-        aip1_expected = [str(aip1.log['Started']), 'aip-1', 'No files deleted', 'Objects folder already exists in original files',
-                         'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'n/a', 'Error during processing\n']
-        aip2_expected = [str(aip2.log['Started']), 'aip-2', 'No files deleted', 'Successfully created objects folder',
-                         'Successfully created metadata folder', 'No FITS tool errors',
-                         'Successfully created combined-fits.xml', 'Successfully created preservation.xml',
-                         'Preservation.xml is not valid (see log in error folder)', 'n/a', 'n/a', 'n/a',
-                         'Error during processing\n']
-        aip3_expected = [str(aip3.log['Started']), 'aip-3', 'No files deleted', 'Successfully created objects folder',
-                         'Successfully created metadata folder', 'No FITS tool errors',
-                         'Successfully created combined-fits.xml', 'Successfully created preservation.xml',
-                         'Preservation.xml valid on 2022-10-31 13:14:15.123456',
-                         'Bag valid on 2022-10-13 14:15:16.789123', 'Successfully made package',
-                         'Successfully added AIP to manifest.', 'Successfully completed processing\n']
-        expected = [",".join(header_expected), ",".join(aip1_expected), ",".join(aip2_expected), ",".join(aip3_expected)]
+        # Time from the log is used for an exact comparison.
+        # Time changes each time the rest is run. The rest of the expected values are the same each time.
+        aip1_row = f'{str(aip1.log["Started"])},aip-1,No files deleted,Objects folder already exists in original ' \
+                   f'files,n/a,n/a,n/a,n/a,n/a,n/a,n/a,n/a,Error during processing\n'
+
+        aip2_row = f'{str(aip2.log["Started"])},aip-2,No files deleted,Successfully created objects folder,' \
+                   f'Successfully created metadata folder,' \
+                   f'No FITS tool errors,Successfully created combined-fits.xml,Successfully created ' \
+                   f'preservation.xml,Preservation.xml is not valid (see log in error folder),n/a,n/a,n/a,' \
+                   f'Error during processing\n'
+
+        aip3_row = f'{str(aip3.log["Started"])},aip-3,No files deleted,Successfully created objects folder,' \
+                   f'Successfully created metadata folder,' \
+                   f'No FITS tool errors,Successfully created combined-fits.xml,Successfully created ' \
+                   f'preservation.xml,Preservation.xml valid on 2022-10-31 13:14:15.123456,Bag valid on 2022-10-13 ' \
+                   f'14:15:16.789123,Successfully made package,Successfully added AIP to manifest.,Successfully ' \
+                   f'completed processing\n'
+        expected = [self.header, aip1_row, aip2_row, aip3_row]
+
         self.assertEqual(expected, result, 'Problem with multiple aips')
 
 

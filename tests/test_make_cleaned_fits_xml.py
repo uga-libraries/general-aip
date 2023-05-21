51294db51c17104f962dcc3b07c0a3dfc9c4e642
Adriane Hanson <ahansonuga@gmail.com>
2023-05-21 16:11:30 -0400

    Clean up test function
    Add test for moving folder and for log Complete to the error, comments, single quote to double

--- tests/test_make_cleaned_fits_xml.py
+++ tests/test_make_cleaned_fits_xml.py
@@ -15,9 +15,9 @@ class MyTestCase(unittest.TestCase):
         """
         make_output_directories()
 
-        self.aip = AIP(os.getcwd(), 'test', 'coll-1', 'aip-folder', 'aip-id', 'title', 1, True)
+        self.aip = AIP(os.getcwd(), "test", "coll-1", "aip-folder", "aip-id", "title", 1, True)
         os.mkdir(self.aip.id)
-        with open(os.path.join(self.aip.id, 'file.txt'), 'w') as file:
+        with open(os.path.join(self.aip.id, "file.txt"), "w") as file:
             file.write("Test text")
 
         structure_directory(self.aip)
@@ -29,20 +29,16 @@ class MyTestCase(unittest.TestCase):
         If they are present, deletes the test AIP, the AIP log, the errors folder, and the script output folders.
         """
 
-        if os.path.exists('aip-id'):
-            shutil.rmtree('aip-id')
+        if os.path.exists("aip-id"):
+            shutil.rmtree("aip-id")
 
-        log_path = os.path.join('..', 'aip_log.csv')
+        log_path = os.path.join("..", "aip_log.csv")
         if os.path.exists(log_path):
             os.remove(log_path)
 
-        errors_path = os.path.join('..', 'errors')
-        if os.path.exists(errors_path):
-            shutil.rmtree(errors_path)
-
-        script_output_folders = ('aips-to-ingest', 'fits-xml', 'preservation-xml')
+        script_output_folders = ("aips-to-ingest", "errors", "fits-xml", "preservation-xml")
         for folder in script_output_folders:
-            path = os.path.join('..', folder)
+            path = os.path.join("..", folder)
             if os.path.exists(path):
                 shutil.rmtree(path)
 
@@ -54,39 +50,45 @@ class MyTestCase(unittest.TestCase):
         make_cleaned_fits_xml(self.aip)
 
         # Edits the timestamp and filepath to consistent values to allow an exact comparison to expected.
-        tree = ET.parse(os.path.join('aip-id', 'metadata', 'aip-id_cleaned-fits.xml'))
+        tree = ET.parse(os.path.join("aip-id", "metadata", "aip-id_cleaned-fits.xml"))
         root = tree.getroot()
-        for fits in root.iter('{http://hul.harvard.edu/ois/xml/ns/fits/fits_output}fits'):
-            fits.set('timestamp', '01/01/22 1:23 PM')
-        for filepath in root.iter('{http://hul.harvard.edu/ois/xml/ns/fits/fits_output}filepath'):
-            filepath.text = 'C:/Path/aip-id/objects/file.txt'
-        tree.write(os.path.join('aip-id', 'metadata', 'aip-id_cleaned-fits.xml'),
-                   xml_declaration=True, encoding='UTF-8')
+        for fits in root.iter("{http://hul.harvard.edu/ois/xml/ns/fits/fits_output}fits"):
+            fits.set("timestamp", "01/01/22 1:23 PM")
+        for filepath in root.iter("{http://hul.harvard.edu/ois/xml/ns/fits/fits_output}filepath"):
+            filepath.text = "C:/Path/aip-id/objects/file.txt"
+        tree.write(os.path.join("aip-id", "metadata", "aip-id_cleaned-fits.xml"),
+                   xml_declaration=True, encoding="UTF-8")
 
         # Reads the cleaned-fits.xml file produced by the function and a cleaned-fits.xml file with the expected values.
-        with open(os.path.join('aip-id', 'metadata', 'aip-id_cleaned-fits.xml'), 'r') as result_file:
+        with open(os.path.join("aip-id", "metadata", "aip-id_cleaned-fits.xml"), "r") as result_file:
             result = result_file.read()
-        with open(os.path.join('expected_xml', 'aip-id_cleaned-fits.xml'), 'r') as expected_file:
+        with open(os.path.join("expected_xml", "aip-id_cleaned-fits.xml"), "r") as expected_file:
             expected = expected_file.read()
 
-        self.assertEqual(result, expected, 'Problem with cleaned fits')
+        self.assertEqual(result, expected, "Problem with cleaned fits")
 
     def test_make_cleaned_fits_xml_error(self):
         """
         Test for error handling while making the cleaned-fits.xml file.
-        Result for testing is the AIP log.
         """
         # Causes the error by deleting the combined-fits.xml file used as the saxon input.
-        os.remove(os.path.join('aip-id', 'metadata', 'aip-id_combined-fits.xml'))
+        os.remove(os.path.join("aip-id", "metadata", "aip-id_combined-fits.xml"))
         make_cleaned_fits_xml(self.aip)
 
-        result = self.aip.log['PresXML']
+        # Test for the AIP folder being moved to the error folder.
+        # Tests both that the folder is in the error folder and not in the AIPs directory.
+        result = (os.path.exists(os.path.join("..", "errors", "cleaned_fits_saxon_error", "aip-id")),
+                  os.path.exists("aip-id"))
+        expected = (True, False)
+        self.assertEqual(result, expected, "Problem with cleaned fits error handling, moving to error folder")
 
-        expected = 'Issue when creating cleaned-fits.xml. ' \
-                   'Saxon error: Source file aip-id\\metadata\\aip-id_combined-fits.xml does not exist\r\n'
+        # Test for the AIP log.
+        result_log = (self.aip.log["PresXML"], self.aip.log["Complete"])
+        expected_log = ("Issue when creating cleaned-fits.xml. " 
+                        "Saxon error: Source file aip-id\\metadata\\aip-id_combined-fits.xml does not exist\r\n",
+                        "Error during processing")
+        self.assertEqual(result_log, expected_log, "Problem with cleaned fits error handling, log")
 
-        self.assertEqual(result, expected, 'Problem with cleaned fits error handling')
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()

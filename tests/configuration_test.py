5987794f84e1292b6544f62df04fc71e7ac17072
Adriane Hanson <ahansonuga@gmail.com>
2024-09-30 11:03:21 -0400

    Add unit test for check_configuration
    Not complete, as it can only test a single variation and requires renaming the file first, so it can't be run as part of the entire test suite. But it is a start.

---
+++ tests/configuration_test.py
@@ -0,0 +1,18 @@
+"""For initial unit testing experiment"""
+
+# Path variables.
+
+# Path does not exist and letter directory does not match AIPS directory.
+FITS = 'Z:\\FITS\\fits.bat'
+
+# Path does not exist.
+SAXON = 'Z:\\Programs\\SaxonHE10-5J\\saxon-he-10.5.jar'
+
+# Missing variable MD5DEEP
+
+# Path exists
+STYLESHEETS = '..\\stylesheets'
+
+# Non-path variables
+# Missing GROUPS
+NAMESPACE = 'https://namespace.edu'

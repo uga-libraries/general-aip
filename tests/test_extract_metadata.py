5ca51ff373b7951ab10b7629f6fc15b0aa3d0c09
Adriane M Hanson <ahansonuga@gmail.com>
2025-07-18 17:02:30 -0400

    Double to single quotes

--- tests/test_extract_metadata.py
+++ tests/test_extract_metadata.py
@@ -31,17 +31,17 @@ class TestExtractMetadata(unittest.TestCase):
         """
         # Makes test input and runs the function being tested.
         aips_dir = os.path.join(os.getcwd(), 'extract_metadata')
-        aip = AIP(aips_dir, "dept", None, "coll-1", "one_folder", "general", "aip-id-one", "title", 1, True)
+        aip = AIP(aips_dir, 'dept', None, 'coll-1', 'one_folder', 'general', 'aip-id-one', 'title', 1, True)
         extract_metadata(aip)
 
         # Test for the contents of the metadata folder.
-        result = os.listdir(os.path.join(aips_dir, "aip-id-one", "metadata"))
-        expected = ["metadata.txt", "Text.txt_fits.xml"]
+        result = os.listdir(os.path.join(aips_dir, 'aip-id-one', 'metadata'))
+        expected = ['metadata.txt', 'Text.txt_fits.xml']
         self.assertEqual(result, expected, "Problem with one file, metadata folder")
 
         # Test for the AIP log.
-        result_log = aip.log["FITSTool"]
-        expected_log = "No FITS tools errors"
+        result_log = aip.log['FITSTool']
+        expected_log = 'No FITS tools errors'
         self.assertEqual(result_log, expected_log, "Problem with one file, log")
 
     def test_multiple_files(self):
@@ -50,17 +50,17 @@ class TestExtractMetadata(unittest.TestCase):
         """
         # Makes test input and runs the function being tested.
         aips_dir = os.path.join(os.getcwd(), 'extract_metadata')
-        aip = AIP(aips_dir, "dept", None, "coll-1", "multi_folder", "general", "aip-id-multi", "title", 1, True)
+        aip = AIP(aips_dir, 'dept', None, 'coll-1', 'multi_folder', 'general', 'aip-id-multi', 'title', 1, True)
         extract_metadata(aip)
 
         # Test for the contents of the metadata folder.
-        result = os.listdir(os.path.join(aips_dir, "aip-id-multi", "metadata"))
-        expected = ["metadata.txt", "output.csv_fits.xml", "output.json_fits.xml", "Text.txt_fits.xml"]
+        result = os.listdir(os.path.join(aips_dir, 'aip-id-multi', 'metadata'))
+        expected = ['metadata.txt', 'output.csv_fits.xml', 'output.json_fits.xml', 'Text.txt_fits.xml']
         self.assertEqual(result, expected, "Problem with multiple files, metadata folder")
 
         # Test for the AIP log.
-        result_log = aip.log["FITSTool"]
-        expected_log = "No FITS tools errors"
+        result_log = aip.log['FITSTool']
+        expected_log = 'No FITS tools errors'
         self.assertEqual(result_log, expected_log, "Problem with multiple files, log")
 
     def test_error_fits_tool(self):
@@ -69,27 +69,27 @@ class TestExtractMetadata(unittest.TestCase):
         """
         # Makes test input and runs the function being tested.
         aips_dir = os.path.join(os.getcwd(), 'extract_metadata')
-        aip = AIP(aips_dir, "dept", None, "coll-1", "tool_error_folder", "general", "aip-id-error", "title", 1, True)
+        aip = AIP(aips_dir, 'dept', None, 'coll-1', 'tool_error_folder', 'general', 'aip-id-error', 'title', 1, True)
         extract_metadata(aip)
 
         # Test for the contents of the metadata folder.
-        result = os.listdir(os.path.join(aips_dir, "aip-id-error", "metadata"))
-        expected = ["aip-id-error_fits-tool-errors_fitserr.txt", "metadata.txt", "not.xml_fits.xml"]
+        result = os.listdir(os.path.join(aips_dir, 'aip-id-error', 'metadata'))
+        expected = ['aip-id-error_fits-tool-errors_fitserr.txt', 'metadata.txt', 'not.xml_fits.xml']
         self.assertEqual(result, expected, "Problem with error, metadata folder")
 
         # Test for the FITS error log, which is if 3 phrases are in the file.
         # The contents of the entire file cannot be tested, since most are variable (timestamps and file paths).
-        with open(os.path.join(aips_dir, "aip-id-error", "metadata", "aip-id-error_fits-tool-errors_fitserr.txt")) as file:
+        with open(os.path.join(aips_dir, 'aip-id-error', 'metadata', 'aip-id-error_fits-tool-errors_fitserr.txt')) as file:
             content = file.read()
-            result = ("org.jdom.input.JDOMParseException" in content,
-                      "Tool error processing file" in content,
-                      "Content is not allowed in prolog." in content)
+            result = ('org.jdom.input.JDOMParseException' in content,
+                      'Tool error processing file' in content,
+                      'Content is not allowed in prolog.' in content)
         expected = (True, True, True)
         self.assertEqual(result, expected, "Problem with error, FITS tool log")
 
         # Test for the AIP log.
-        result_log = aip.log["FITSTool"]
-        expected_log = "FITS tools generated errors (saved to metadata folder)"
+        result_log = aip.log['FITSTool']
+        expected_log = 'FITS tools generated errors (saved to metadata folder)'
         self.assertEqual(result_log, expected_log, "Problem with error, AIP log")
 
 

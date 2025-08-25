9188c86abfdbae045a9ae50e10dfcc1686af4342
Adriane M Hanson <ahansonuga@gmail.com>
2025-08-25 11:07:06 -0400

    Make work with different OS output

--- tests/test_delete_temp.py
+++ tests/test_delete_temp.py
@@ -43,10 +43,12 @@ def deletion_log_rows(log_path):
 
 class TestDeleteTemp(unittest.TestCase):
 
+    def setUp(self):
+        """Date this repo was first cloned to local machine, which is the date info in the deletion log."""
+        self.modified = '2025-8-14'
+
     def tearDown(self):
-        """
-        Deletes the AIP folders created by each test.
-        """
+        """Deletes the AIP folders created by each test"""
         aip_folders = ['aip-id_dot', 'aip-id_ds-store', 'aip-id_ds-store-2', 'aip-id_tmp', 'aip-id_thumbs']
         for aip_folder in aip_folders:
             aip_path = os.path.join(os.getcwd(), 'delete_temp', aip_folder)
@@ -54,9 +56,7 @@ class TestDeleteTemp(unittest.TestCase):
                 shutil.rmtree(aip_path)
 
     def test_no_temp(self):
-        """
-        Test for an AIP with no temporary files to delete.
-        """
+        """Test for an AIP with no temporary files to delete"""
         # Makes the input needed for the function and runs the function being tested.
         aips_dir = os.path.join(os.getcwd(), 'delete_temp')
         aip = AIP(aips_dir, 'dept', None, 'coll-1', 'folder', 'general', 'aip-id', 'title', 1, True)
@@ -71,14 +71,12 @@ class TestDeleteTemp(unittest.TestCase):
         self.assertEqual(result, expected, "Problem with test for no temporary files, AIP folder")
 
         # Test for the AIP log.
-        result_aip_log = aip.log['Deletions']
-        expected_aip_log = 'No files deleted'
-        self.assertEqual(result_aip_log, expected_aip_log, "Problem with test for no temporary files, AIP log")
+        result = aip.log['Deletions']
+        expected = 'No files deleted'
+        self.assertEqual(result, expected, "Problem with test for no temporary files, AIP log")
 
     def test_ds_store(self):
-        """
-        Test for an AIP with .DS_Store files to delete.
-        """
+        """Test for an AIP with .DS_Store files to delete"""
         # Makes the input needed for the function and runs the function being tested.
         aips_dir = os.path.join(os.getcwd(), 'delete_temp')
         shutil.copytree(os.path.join(aips_dir, 'aip-id_ds-store_copy'), os.path.join(aips_dir, 'aip-id_ds-store'))
@@ -99,22 +97,19 @@ class TestDeleteTemp(unittest.TestCase):
         self.assertEqual(result, expected, "Problem with test for .DS_Store, AIP folder")
 
         # Test for the AIP log.
-        result_aip_log = aip.log['Deletions']
-        expected_aip_log = 'File(s) deleted'
-        self.assertEqual(result_aip_log, expected_aip_log, "Problem with test for .DS_Store, AIP log")
+        result = aip.log['Deletions']
+        expected = 'File(s) deleted'
+        self.assertEqual(result, expected, "Problem with test for .DS_Store, AIP log")
 
         # Test for the deletion log.
-        result_del_log = deletion_log_rows(deletion_log)
-        expected_del_log = [['Path', 'File Name', 'Size (Bytes)', 'Date Last Modified'],
-                            [os.path.join(aips_dir, 'aip-id_ds-store', '.DS_Store'), '.DS_Store', 0, '2025-8-14'],
-                            [os.path.join(aips_dir, 'aip-id_ds-store', 'Test Dir', '.DS_Store'), '.DS_Store', 0,
-                             '2025-8-14']]
-        self.assertEqual(result_del_log, expected_del_log, "Problem with test for .DS_Store, deletion log")
+        result = deletion_log_rows(deletion_log)
+        expected = [['Path', 'File Name', 'Size (Bytes)', 'Date Last Modified'],
+                    [os.path.join(aips_dir, 'aip-id_ds-store', '.DS_Store'), '.DS_Store', 0, self.modified],
+                    [os.path.join(aips_dir, 'aip-id_ds-store', 'Test Dir', '.DS_Store'), '.DS_Store', 0, self.modified]]
+        self.assertEqual(result, expected, "Problem with test for .DS_Store, deletion log")
 
     def test_ds_store_2(self):
-        """
-        Test for an AIP with ._.DS_Store files to delete.
-        """
+        """Test for an AIP with ._.DS_Store files to delete"""
         # Makes the input needed for the function and runs the function being tested.
         aips_dir = os.path.join(os.getcwd(), 'delete_temp')
         shutil.copytree(os.path.join(aips_dir, 'aip-id_ds-store-2_copy'), os.path.join(aips_dir, 'aip-id_ds-store-2'))
@@ -135,22 +130,20 @@ class TestDeleteTemp(unittest.TestCase):
         self.assertEqual(result, expected, "Problem with test for ._.DS_Store, AIP folder")
 
         # Test for the AIP log.
-        result_aip_log = aip.log['Deletions']
-        expected_aip_log = 'File(s) deleted'
-        self.assertEqual(result_aip_log, expected_aip_log, "Problem with test for ._.DS_Store, AIP log")
+        result = aip.log['Deletions']
+        expected = 'File(s) deleted'
+        self.assertEqual(result, expected, "Problem with test for ._.DS_Store, AIP log")
 
         # Test for the deletion log.
-        result_del_log = deletion_log_rows(deletion_log)
-        expected_del_log = [['Path', 'File Name', 'Size (Bytes)', 'Date Last Modified'],
-                            [os.path.join(aips_dir, 'aip-id_ds-store-2', '._.DS_Store'), '._.DS_Store', 0, '2025-8-14'],
-                            [os.path.join(aips_dir, 'aip-id_ds-store-2', 'Test Dir', '._.DS_Store'),
-                             '._.DS_Store', 0, '2025-8-14']]
-        self.assertEqual(result_del_log, expected_del_log, "Problem with test for .DS_Store, deletion log")
+        result = deletion_log_rows(deletion_log)
+        expected = [['Path', 'File Name', 'Size (Bytes)', 'Date Last Modified'],
+                    [os.path.join(aips_dir, 'aip-id_ds-store-2', '._.DS_Store'), '._.DS_Store', 0, self.modified],
+                    [os.path.join(aips_dir, 'aip-id_ds-store-2', 'Test Dir', '._.DS_Store'), '._.DS_Store', 0,
+                     self.modified]]
+        self.assertEqual(result, expected, "Problem with test for .DS_Store, deletion log")
 
     def test_thumbs_db(self):
-        """
-        Test for an AIP with Thumbs.db files to delete.
-        """
+        """Test for an AIP with Thumbs.db files to delete"""
         # Makes the input needed for the function and runs the function being tested.
         aips_dir = os.path.join(os.getcwd(), 'delete_temp')
         shutil.copytree(os.path.join(aips_dir, 'aip-id_thumbs_copy'), os.path.join(aips_dir, 'aip-id_thumbs'))
@@ -171,22 +164,25 @@ class TestDeleteTemp(unittest.TestCase):
         self.assertEqual(result, expected, "Problem with test for Thumbs.db, AIP folder")
 
         # Test for the AIP log.
-        result_aip_log = aip.log['Deletions']
-        expected_aip_log = 'File(s) deleted'
-        self.assertEqual(result_aip_log, expected_aip_log, "Problem with test for Thumbs.db, AIP log")
+        result = aip.log['Deletions']
+        expected = 'File(s) deleted'
+        self.assertEqual(result, expected, "Problem with test for Thumbs.db, AIP log")
 
         # Test for the deletion log.
-        result_del_log = deletion_log_rows(deletion_log)
-        expected_del_log = [['Path', 'File Name', 'Size (Bytes)', 'Date Last Modified'],
-                            [os.path.join(aips_dir, 'aip-id_thumbs', 'Thumbs.db'), 'Thumbs.db', 25, '2025-8-14'],
-                            [os.path.join(aips_dir, 'aip-id_thumbs', 'Test Dir', 'Thumbs.db'),
-                             'Thumbs.db', 2590, '2025-8-14']]
-        self.assertEqual(result_del_log, expected_del_log, "Problem with test for Thumbs.db, deletion log")
+        # Size is different depending on the OS.
+        result = deletion_log_rows(deletion_log)
+        expected = [[['Path', 'File Name', 'Size (Bytes)', 'Date Last Modified'],
+                     [os.path.join(aips_dir, 'aip-id_thumbs', 'Thumbs.db'), 'Thumbs.db', 25, self.modified],
+                     [os.path.join(aips_dir, 'aip-id_thumbs', 'Test Dir', 'Thumbs.db'), 'Thumbs.db', 2590,
+                      self.modified]],
+                    [['Path', 'File Name', 'Size (Bytes)', 'Date Last Modified'],
+                     [os.path.join(aips_dir, 'aip-id_thumbs', 'Thumbs.db'), 'Thumbs.db', 25, self.modified],
+                     [os.path.join(aips_dir, 'aip-id_thumbs', 'Test Dir', 'Thumbs.db'), 'Thumbs.db', 2595,
+                      self.modified]]]
+        self.assertIn(result, expected, "Problem with test for Thumbs.db, deletion log")
 
     def test_dot_prefix(self):
-        """
-        Test for an AIP with .filename files to delete.
-        """
+        """Test for an AIP with .filename files to delete"""
         # Makes the input needed for the function and runs the function being tested.
         aips_dir = os.path.join(os.getcwd(), 'delete_temp')
         shutil.copytree(os.path.join(aips_dir, 'aip-id_dot_copy'), os.path.join(aips_dir, 'aip-id_dot'))
@@ -207,22 +203,19 @@ class TestDeleteTemp(unittest.TestCase):
         self.assertEqual(result, expected, "Problem with test for dot prefix, AIP folder")
 
         # Test for the AIP log.
-        result_aip_log = aip.log['Deletions']
-        expected_aip_log = 'File(s) deleted'
-        self.assertEqual(result_aip_log, expected_aip_log, "Problem with test for dot prefix, AIP log")
+        result = aip.log['Deletions']
+        expected = 'File(s) deleted'
+        self.assertEqual(result, expected, "Problem with test for dot prefix, AIP log")
 
         # Test for the deletion log.
-        result_del_log = deletion_log_rows(deletion_log)
-        expected_del_log = [['Path', 'File Name', 'Size (Bytes)', 'Date Last Modified'],
-                            [os.path.join(aips_dir, 'aip-id_dot', '.temp.txt'), '.temp.txt', 0, '2025-8-14'],
-                            [os.path.join(aips_dir, 'aip-id_dot', 'Test Dir', '.temp.txt'), '.temp.txt', 0,
-                             '2025-8-14']]
-        self.assertEqual(result_del_log, expected_del_log, "Problem with test for dot prefix, deletion log")
+        result = deletion_log_rows(deletion_log)
+        expected = [['Path', 'File Name', 'Size (Bytes)', 'Date Last Modified'],
+                    [os.path.join(aips_dir, 'aip-id_dot', '.temp.txt'), '.temp.txt', 0, self.modified],
+                    [os.path.join(aips_dir, 'aip-id_dot', 'Test Dir', '.temp.txt'), '.temp.txt', 0, self.modified]]
+        self.assertEqual(result, expected, "Problem with test for dot prefix, deletion log")
 
     def test_tmp_extension(self):
-        """
-        Test for an AIP with filename.tmp files to delete.
-        """
+        """Test for an AIP with filename.tmp files to delete"""
         # Makes the input needed for the function and runs the function being tested.
         aips_dir = os.path.join(os.getcwd(), 'delete_temp')
         shutil.copytree(os.path.join(aips_dir, 'aip-id_tmp_copy'), os.path.join(aips_dir, 'aip-id_tmp'))
@@ -243,17 +236,22 @@ class TestDeleteTemp(unittest.TestCase):
         self.assertEqual(result, expected, "Problem with test for .tmp extension, AIP folder")
 
         # Test for the AIP log.
-        result_aip_log = aip.log['Deletions']
-        expected_aip_log = 'File(s) deleted'
-        self.assertEqual(result_aip_log, expected_aip_log, "Problem with test for .tmp extension, AIP log")
+        result = aip.log['Deletions']
+        expected = 'File(s) deleted'
+        self.assertEqual(result, expected, "Problem with test for .tmp extension, AIP log")
 
         # Test for the deletion log.
-        result_del_log = deletion_log_rows(deletion_log)
-        expected_del_log = [['Path', 'File Name', 'Size (Bytes)', 'Date Last Modified'],
-                            [os.path.join(aip_path, 'Text.tmp'), 'Text.tmp', 9, '2025-8-14'],
-                            [os.path.join(aip_path, 'Test Dir', 'Test Dir Text.tmp'),
-                             'Test Dir Text.tmp', 1615, '2025-8-14']]
-        self.assertEqual(result_del_log, expected_del_log, "Problem with test for .tmp extension, deletion log")
+        # Size is different depending on the OS.
+        result = deletion_log_rows(deletion_log)
+        expected = [[['Path', 'File Name', 'Size (Bytes)', 'Date Last Modified'],
+                     [os.path.join(aip_path, 'Text.tmp'), 'Text.tmp', 9, self.modified],
+                     [os.path.join(aip_path, 'Test Dir', 'Test Dir Text.tmp'), 'Test Dir Text.tmp', 1615,
+                      self.modified]],
+                    [['Path', 'File Name', 'Size (Bytes)', 'Date Last Modified'],
+                     [os.path.join(aip_path, 'Text.tmp'), 'Text.tmp', 9, self.modified],
+                     [os.path.join(aip_path, 'Test Dir', 'Test Dir Text.tmp'), 'Test Dir Text.tmp', 1583,
+                      self.modified]]]
+        self.assertIn(result, expected, "Problem with test for .tmp extension, deletion log")
 
 
 if __name__ == "__main__":

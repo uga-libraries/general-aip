6c7ea2d6a97d2717bb6ed699beb602842807da69
Adriane M Hanson <ahansonuga@gmail.com>
2025-09-08 10:19:48 -0400

    Rename general to non-av for accuracy

--- tests/test_make_output_directories.py
+++ tests/test_make_output_directories.py
@@ -1,5 +1,5 @@
-"""Testing for the function make_output_directories, which creates three directories in the current directory.
-It does not have any inputs and returns nothing.
+"""Testing for the function make_output_directories, which makes directories for output in staging 
+if they don't already exist. Different directories are made depending on the mode.
 
 The function check_configuration() confirms that staging is a valid path, so no test is needed for that error.
 """
@@ -23,9 +23,9 @@ def directory_print(path):
 class TestMakeOutputDirectories(unittest.TestCase):
 
     def tearDown(self):
-        """Deletes the staging folder (copy of data made for each test)"""
+        """Deletes the finished staging folder (copy of data made for each test)"""
         staging_folders = ['staging_av_all', 'staging_av_none', 'staging_av_partial',
-                           'staging_general_all', 'staging_general_none', 'staging_general_partial']
+                           'staging_all', 'staging_none', 'staging_partial']
         for staging_folder in staging_folders:
             staging_path = os.path.join(os.getcwd(), 'make_output_directories', staging_folder)
             if os.path.exists(staging_path):
@@ -91,41 +91,41 @@ class TestMakeOutputDirectories(unittest.TestCase):
                     os.path.join(staging, 'preservation-xmls')]
         self.assertEqual(expected, result, "Problem with av_partial")
 
-    def test_general_all(self):
-        """Test for general mode when all three folders already exist"""
+    def test_non_av_all(self):
+        """Test for mode other than av when all the folders already exist"""
         # Makes a copy of the test input and runs the function.
-        staging = os.path.join(os.getcwd(), 'make_output_directories', 'staging_general_all')
+        staging = os.path.join(os.getcwd(), 'make_output_directories', 'staging_all')
         shutil.copytree(f'{staging}_copy', staging)
-        make_output_directories(staging, 'general')
+        make_output_directories(staging, 'non_av')
 
         # Verifies that staging has the expected folders.
         result = directory_print(staging)
         expected = ['aips-ready-to-ingest', 'fits-xmls', 'preservation-xmls']
-        self.assertEqual(expected, result, "Problem with general_all")
+        self.assertEqual(expected, result, "Problem with non_av_all")
 
-    def test_general_none(self):
-        """Test for general mode when none of the three folders already exist"""
+    def test_non_av_none(self):
+        """Test for mode other than av when none of the folders already exist"""
         # Makes a copy of the test input and runs the function.
-        staging = os.path.join(os.getcwd(), 'make_output_directories', 'staging_general_none')
+        staging = os.path.join(os.getcwd(), 'make_output_directories', 'staging_none')
         shutil.copytree(f'{staging}_copy', staging)
-        make_output_directories(staging, 'general')
+        make_output_directories(staging, 'non_av')
 
         # Verifies that staging has the expected folders.
         result = directory_print(staging)
         expected = ['aips-ready-to-ingest', 'fits-xmls', 'preservation-xmls']
-        self.assertEqual(expected, result, "Problem with general_none")
+        self.assertEqual(expected, result, "Problem with non_av_none")
 
-    def test_general_partial(self):
-        """Test for general mode when one of the three folders (aips-ready-to-ingest) already exists."""
+    def test_non_av_partial(self):
+        """Test for mode other than av when some the three folders already exist."""
         # Makes a copy of the test input and runs the function.
-        staging = os.path.join(os.getcwd(), 'make_output_directories', 'staging_general_partial')
+        staging = os.path.join(os.getcwd(), 'make_output_directories', 'staging_partial')
         shutil.copytree(f'{staging}_copy', staging)
-        make_output_directories(staging, 'general')
+        make_output_directories(staging, 'non_av')
 
         # Verifies that staging has the expected folders.
         result = directory_print(staging)
         expected = ['aips-ready-to-ingest', 'fits-xmls', 'preservation-xmls']
-        self.assertEqual(expected, result, "Problem with general_partial")
+        self.assertEqual(expected, result, "Problem with non_av_partial")
 
 
 if __name__ == "__main__":

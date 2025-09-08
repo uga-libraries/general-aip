21e354f0b7b1eb880b22c521606acaf5163430ac
Adriane M Hanson <ahansonuga@gmail.com>
2025-09-08 17:22:27 -0400

    Simplify docstrings

--- tests/test_check_metadata_csv.py
+++ tests/test_check_metadata_csv.py
@@ -11,9 +11,7 @@ from aip_functions import check_metadata_csv
 
 
 def run_function(csv_name):
-    """
-    Reads the CSV, runs the function with the read data, and returns the function output.
-    """
+    """Reads the CSV, runs the function with the read data, and returns the function output"""
     aip_dir = os.path.join(os.getcwd(), 'check_metadata_csv')
     with open(os.path.join(aip_dir, csv_name)) as open_metadata:
         read_metadata = csv.reader(open_metadata)
@@ -24,27 +22,19 @@ def run_function(csv_name):
 class TestCheckMetadataCSV(unittest.TestCase):
 
     def test_correct(self):
-        """
-        Test for a metadata.csv with valid information 
-        and column case matches the README instructions exactly.
-        """
+        """Test for a metadata.csv with the correct information (column case matches)"""
         result = run_function('correct_same_case_metadata.csv')
         expected = []
         self.assertEqual(expected, result, "Problem with test for correct_same_case_metadata.csv")
 
     def test_correct_case_difference(self):
-        """
-        Test for a metadata.csv with valid information
-        and the column case doesn't match the README instructions. This is not an error.
-        """
+        """Test for a metadata.csv with the correct information (column case different)"""
         result = run_function('correct_diff_case_metadata.csv')
         expected = []
         self.assertEqual(expected, result, "Problem with test for correct_diff_case_metadata.csv")
 
     def test_error_columns(self):
-        """
-        Test for the column names in metadata.csv not matching the expected values.
-        """
+        """Test for the column names in metadata.csv not matching the expected values"""
         result = run_function('error_column_order_metadata.csv')
         expected = ['The columns in the metadata.csv do not match the required values or order.',
                     'Required: Department, Collection, Folder, AIP_ID, Title, Version',
@@ -53,35 +43,27 @@ class TestCheckMetadataCSV(unittest.TestCase):
         self.assertEqual(expected, result, "Problem with test for error, columns")
 
     def test_error_group(self):
-        """
-        Test for departments in the metadata.csv which are not ARCHive groups (from the configuration file).
-        """
+        """Test for departments in the metadata.csv which are not ARCHive groups (from the configuration file)"""
         result = run_function('error_group_metadata.csv')
         expected = ['Brown is not an ARCHive group.', 'banana is not an ARCHive group.']
         self.assertEqual(expected, result, "Problem with test for error, group")
 
     def test_error_repeated_folders(self):
-        """
-        Test for AIPs that are in the metadata.csv more than once.
-        """
+        """Test for AIPs that are in the metadata.csv more than once"""
         result = run_function('error_repeat_metadata.csv')
         expected = ['aip-2 is in the metadata.csv folder column more than once.',
                     'aip-3 is in the metadata.csv folder column more than once.']
         self.assertEqual(expected, result, "Problem with test for error, repeated folders")
 
     def test_error_csv_only(self):
-        """
-        Test for AIP folders that are only in the metadata.csv and not in the AIPs directory.
-        """
+        """Test for AIP folders that are only in the metadata.csv and not in the AIPs directory"""
         result = run_function('error_csv_only_metadata.csv')
         expected = ['aip-4 is in metadata.csv and missing from the AIPs directory.',
                     'aip-5 is in metadata.csv and missing from the AIPs directory.']
         self.assertEqual(expected, result, "Problem with test for error, CSV only")
 
     def test_error_directory_only(self):
-        """
-        Test for AIP folders that are only in the AIPs directory and not the metadata.csv.
-        """
+        """Test for AIP folders that are only in the AIPs directory and not the metadata.csv"""
         result = run_function('error_directory_only_metadata.csv')
         expected = ['aip-1 is in the AIPs directory and missing from metadata.csv.',
                     'aip-3 is in the AIPs directory and missing from metadata.csv.']

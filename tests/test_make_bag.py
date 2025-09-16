bf32519649a103ba6472d6de0b48e390a88f4b37
Adriane M Hanson <ahansonuga@gmail.com>
2025-09-16 16:24:17 -0400

    Add test for make_bag() with temp files

--- tests/test_make_bag.py
+++ tests/test_make_bag.py
@@ -14,7 +14,7 @@ class TestMakeBag(unittest.TestCase):
     def tearDown(self):
         """If they are present, deletes the test AIPs if correctly renamed to add "_bag" or not"""
         aips_dir = os.path.join(os.getcwd(), 'make_bag')
-        aips = ['rabbitbox_0001', 'rbrl_025_er_000001', 'rbrl025_0001_media']
+        aips = ['rabbitbox_0001', 'rbrl_025_er_000001', 'rbrl025_0001_media', 'test_001_01']
         for aip in aips:
             if os.path.exists(os.path.join(aips_dir, aip)):
                 shutil.rmtree(os.path.join(aips_dir, aip))
@@ -37,7 +37,6 @@ class TestMakeBag(unittest.TestCase):
                     os.path.join(aips_dir, f'{aip.id}_bag', 'bagit.txt'),
                     os.path.join(aips_dir, f'{aip.id}_bag', 'data'),
                     os.path.join(aips_dir, f'{aip.id}_bag', 'data', 'Placeholder for AIP content.txt'),
-                    os.path.join(aips_dir, f'{aip.id}_bag', 'data', f'{aip.id}_files-deleted_{date}_del.csv'),
                     os.path.join(aips_dir, f'{aip.id}_bag', 'manifest-md5.txt'),
                     os.path.join(aips_dir, f'{aip.id}_bag', 'tagmanifest-md5.txt')]
         self.assertEqual(expected, result, "Problem with av_bmac")
@@ -83,6 +82,27 @@ class TestMakeBag(unittest.TestCase):
                     os.path.join(aips_dir, f'{aip.id}_bag', 'tagmanifest-md5.txt'),
                     os.path.join(aips_dir, f'{aip.id}_bag', 'tagmanifest-sha256.txt')]
         self.assertEqual(expected, result, "Problem with general")
+
+    def test_temp(self):
+        """Test for making a bag out of an AIP folder that has temp files that should be deleted"""
+        # Makes the test input and runs the function.
+        # A copy of the AIP is made since this test should move it to an error folder.
+        aips_dir = os.path.join(os.getcwd(), 'make_bag')
+        aip = AIP(aips_dir, 'test', None, 'test_001', 'folder', 'general', 'test_001_01', 'title', 1, True)
+        shutil.copytree(os.path.join(aips_dir, f'{aip.id}_copy'), os.path.join(aips_dir, aip.id))
+        make_bag(aip)
+
+        # Verifies the AIP is now a bag, based on the folder name and contents, with none of the temp files.
+        result = make_directory_list(os.path.join(aips_dir, f'{aip.id}_bag'))
+        expected = [os.path.join(aips_dir, f'{aip.id}_bag', 'bag-info.txt'),
+                    os.path.join(aips_dir, f'{aip.id}_bag', 'bagit.txt'),
+                    os.path.join(aips_dir, f'{aip.id}_bag', 'data'),
+                    os.path.join(aips_dir, f'{aip.id}_bag', 'data', 'Placeholder for AIP content.txt'),
+                    os.path.join(aips_dir, f'{aip.id}_bag', 'manifest-md5.txt'),
+                    os.path.join(aips_dir, f'{aip.id}_bag', 'manifest-sha256.txt'),
+                    os.path.join(aips_dir, f'{aip.id}_bag', 'tagmanifest-md5.txt'),
+                    os.path.join(aips_dir, f'{aip.id}_bag', 'tagmanifest-sha256.txt')]
+        self.assertEqual(expected, result, "Problem with temp")
 
 
 if __name__ == "__main__":

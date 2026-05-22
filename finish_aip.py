7f304978889cc2a34d2a2e459efdafa2ab4f533a
Adriane M Hanson <ahansonuga@gmail.com>
2026-05-22 11:41:04 -0400

    Tar, add size to filename, and zip

--- finish_aip.py
+++ finish_aip.py
@@ -14,7 +14,41 @@ Returns:
     manifest.txt with the md5 of the AIP needed for ingest
 """
 import bagit
+import os
+import subprocess
 import sys
+
+
+def package_bag(bag_path, bag):
+    """Tar, add the size to the filename, and bz2
+    Parameters:
+        bag_path (string) - path to bag
+        bag (bagit Bag) - bag instance for extracting bag payload
+    Returns: None
+    """
+
+    # Gets the total size in bytes of the bag: bag payload (data folder) from bag-info.txt plus bag metadata files.
+    bag_payload = bag.info['Payload-Oxum']
+    bag_size = bag_payload.load.split('.')[0]
+    for file in os.listdir(bag_path):
+        if file.endswith(".txt"):
+            bag_size += os.path.getsize(os.path.join(bag_path, file))
+    bag_size = int(bag_size)
+
+    # Tars the file without printing progress to the terminal (stdout).
+    subprocess.run(f'"C:/Program Files/7-Zip/7z.exe" -ttar a "{bag_path}.tar" "{bag_path}"',
+                   stdout=subprocess.DEVNULL, shell=True)
+
+    # Renames the file to include the size.
+    tar_size_path = f"{bag_path}.{bag_size}.tar"
+    os.replace(f"{bag_path}.tar", tar_size_path)
+
+    # Zips (bz2) the tar file without printing progress to the terminal (stdout).
+    subprocess.run(f'"C:/Program Files/7-Zip/7z.exe" -tbzip2 a -aoa "{tar_size_path}.bz2" "{tar_size_path}"',
+                   stdout=subprocess.DEVNULL, shell=True)
+
+    # Deletes the tar version. Just want the tarred and zipped version.
+    os.remove(tar_size_path)
 
 
 def validate_bag(bag):
